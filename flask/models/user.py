ca5a9ecfac78bcfbe82af35f8299cb1a39e270f0
Evan Dong <evandong777@gmail.com>
2024-10-17 23:13:00 -0400

    just testing. Tested venv and running locally. Also created user.py in model with short comments

---
+++ flask/models/user.py
@@ -0,0 +1,4 @@
+#represent the user's (account's) info
+#should store id, username, password, and maybe other user info
+
+#if doing recipe finder, should also store saved recipes, and idk what else
