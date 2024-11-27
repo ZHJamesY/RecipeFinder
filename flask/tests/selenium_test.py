ee5a0b0425422157d82f6583f3d9d9922dcb7caa
1195James <1195771246y@gmail.com>
2024-11-27 14:13:07 -0500

    + selenium chrome headless option

--- flask/tests/selenium_test.py
+++ flask/tests/selenium_test.py
@@ -34,7 +34,7 @@ def test_app():
 @pytest.fixture(scope='module')
 def driver():
     chrome_options = Options()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
