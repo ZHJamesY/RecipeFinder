45e5445d86a0df3bc32c4328eb086e1022a05d97
1195@JamesYZH <1195771246y@gmail.com>
2024-11-28 09:31:21 -0500

    + pytest

--- flask/run.py
+++ flask/run.py
@@ -24,7 +24,7 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="8000")
+    # app.run(debug=True, host="0.0.0.0", port="8000")
 
     # for local testing with https
-    # app.run(ssl_context="adhoc", debug=True)
+    app.run(ssl_context="adhoc", debug=True)
